bbaa38a21f2cc4d50b1b13dd362f85a922b44863
aakash! <workforxynax@gmail.com>
2025-02-20 21:42:18 +0530

    Added Leetcode278

---
+++ Leetcode278/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        int  ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // if (isBadVersion(mid)) { 
+            //     ans = mid;
+            //     right = mid - 1; // uncomment this 
+            // }
+            // else {
+            //     left = mid + 1;
+            // }
+        }
+        return ans;
+    }
+};
